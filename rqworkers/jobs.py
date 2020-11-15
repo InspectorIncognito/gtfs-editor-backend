d3ea4d2f994e3f8bacfe7c635f2f0f8b6bddff82
Felipinbombin <cephei.1313@gmail.com>
2020-11-15 00:25:58 -0300

    increase timeout to 12 hours

--- rqworkers/jobs.py
+++ rqworkers/jobs.py
@@ -6,7 +6,7 @@ from django_rq import job
 logger = logging.getLogger(__name__)
 
 
-@job(settings.GTFSEDITOR_QUEUE_NAME)
+@job(settings.GTFSEDITOR_QUEUE_NAME, timeout=60 * 60 * 12)
 def gtfseditor_jobk(payload_data):
     import time
     time.sleep(15)
