6678c77faa474b6ad0a8a455e10e9568b6935e83
Felipinbombin <cephei.1313@gmail.com>
2020-11-22 07:21:40 -0300

    added static and media urls to urls.py in debug mode

--- gtfseditor/urls.py
+++ gtfseditor/urls.py
@@ -13,11 +13,12 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework_nested import routers
-from rest_api import views as api_views
 
+from rest_api import views as api_views
 
 router = routers.SimpleRouter()
 router.register(r'projects', api_views.ProjectViewSet)
@@ -41,8 +42,6 @@ project_router.register(r'trips', api_views.TripViewSet, basename='project-trips
 project_router.register(r'stoptimes', api_views.StopTimeViewSet, basename='project-stoptimes')
 project_router.register(r'frequencies', api_views.FrequencyViewSet, basename='project-frequencies')
 
-
-
 urlpatterns = [
     path(r'admin/', admin.site.urls),
     path(r'django-rq/', include('django_rq.urls')),
@@ -50,3 +49,10 @@ urlpatterns = [
     path(r'api/', include(project_router.urls)),
     path(r'api-auth/', include('rest_framework.urls', namespace='rest_framework')),
 ]
+
+if settings.DEBUG:
+    # it serves media files in dev server
+    from django.conf.urls.static import static
+
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
