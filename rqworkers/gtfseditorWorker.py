2e3a0a802fe6b7cc1aab3b80e803287fdcbd7e19
cephe <cephei.1313@gmail.com>
2023-12-19 17:07:57 +0000

    fix worker params

--- rqworkers/gtfseditorWorker.py
+++ rqworkers/gtfseditorWorker.py
@@ -7,11 +7,11 @@ class GTFSEditorWorker(Worker):
 
     def __init__(self, queues, name=None, default_result_ttl=None, connection=None,
                  exc_handler=None, exception_handlers=None, default_worker_ttl=None,
-                 job_class=None, queue_class=None):
+                 job_class=None, queue_class=None, serializer=None):
         print("Initializing GTFSEditorWorker...")
         super(GTFSEditorWorker, self).__init__(queues, name=name, default_result_ttl=default_result_ttl,
                                                connection=connection, exc_handler=exc_handler,
                                                exception_handlers=exception_handlers,
                                                default_worker_ttl=default_worker_ttl, job_class=job_class,
-                                               queue_class=queue_class)
+                                               queue_class=queue_class, serializer=None)
         print("GTFSEditorWorker ready!")
