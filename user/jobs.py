a89b09bb5ce9acb03008cbbf794f45291a77740e
Belén <belen.vasquez.n@ug.uchile.cl>
2024-01-23 08:56:04 -0300

    modificación en job.py para mail con html. Avance html

--- user/jobs.py
+++ user/jobs.py
@@ -1,5 +1,6 @@
 from django_rq import job
-from django.core.mail import EmailMessage
+from django.template.loader import render_to_string
+from django.core.mail import EmailMultiAlternatives
 
 from gtfseditor import settings
 from user.models import User
@@ -9,7 +10,12 @@ from user.models import User
 def send_confirmation_email(username, verification_url):
     user = User.objects.get(username=username)
     subject, to = 'Verificación de Email', user.email
-    text_content = f'Haz clic para verificar tu correo electrónico: {verification_url}'
-    msg = EmailMessage(subject, text_content, settings.EMAIL_HOST_USER, [to])
+    text_content = ''
+    html_content = render_to_string('confirmation_email.html',
+                                    context={'name': user.name,
+                                             'username': user.username,
+                                             'form_url': verification_url})
+    msg = EmailMultiAlternatives(subject, text_content, settings.EMAIL_HOST_USER, [to])
+    msg.attach_alternative(html_content, "text/html")
     msg.send()
 
