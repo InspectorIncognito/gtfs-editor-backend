a5ddfa124c4e189116a4a22b6cc05a73c3c359b8
Belén <belen.vasquez.n@ug.uchile.cl>
2024-02-16 00:07:23 -0300

    Todos los test para verificar que la autenticación funciona y que sólo se puede acceder a las vistas si el usuario es dueño del proyecto

--- user/tests/test_permissions.py
+++ user/tests/test_permissions.py
@@ -31,6 +31,7 @@ class UserPermissionTest(BaseTestCase):
             'USER_TOKEN': token
         }
 
+    # User can register if NOT logged in
     def test_register_permission_success(self):
         url = reverse('user-register')
 
@@ -44,6 +45,7 @@ class UserPermissionTest(BaseTestCase):
         response = self.client.post(url, data, headers=self.custom_headers_not_login, format='json')
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
 
+    # User can NOT register if logged in
     def test_register_permission_fail(self):
         url = reverse('user-register')
 
@@ -57,6 +59,7 @@ class UserPermissionTest(BaseTestCase):
         response = self.client.post(url, data, headers=self.custom_headers, format='json')
         self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
 
+    # User can access 'login' view if NOT logged in
     def test_login_permission_success(self):
         url = reverse('user-login')
 
@@ -69,6 +72,7 @@ class UserPermissionTest(BaseTestCase):
         self.user.refresh_from_db()
         self.assertEqual(response.status_code, status.HTTP_200_OK)
 
+    # User can NOT access 'login' view if logged in
     def test_login_permission_fail(self):
         url = reverse('user-login')
 
@@ -82,6 +86,7 @@ class UserPermissionTest(BaseTestCase):
 
         self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
 
+    # User can access 'confirmation mail' view if NOT logged in
     @patch('user.views.User.objects.get')
     def test_confirmation_mail_permission_success(self, mock_get):
         user = Mock()
@@ -92,6 +97,7 @@ class UserPermissionTest(BaseTestCase):
                                    headers=self.custom_headers_not_login)
         self.assertEqual(response.status_code, status.HTTP_302_FOUND)
 
+    # User can NOT access 'confirmation mail' view if logged in
     @patch('user.views.User.objects.get')
     def test_confirmation_mail_permission_fail(self, mock_get):
         user = Mock()
@@ -102,6 +108,7 @@ class UserPermissionTest(BaseTestCase):
                                    headers=self.custom_headers)
         self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
 
+    # User CAN access 'recovery password request' view if logged in
     def test_recovery_password_request_permission_success(self):
         url = reverse('recover-password-request')
         data = {'username': self.user.username}
@@ -111,6 +118,7 @@ class UserPermissionTest(BaseTestCase):
 
         self.assertEqual(response.status_code, status.HTTP_200_OK)
 
+    # User CAN NOT access 'recovery password request' view if NOT logged in
     def test_recovery_password_request_permission_fail(self):
         url = reverse('recover-password-request')
         data = {'username': self.userPW.username}
@@ -120,6 +128,7 @@ class UserPermissionTest(BaseTestCase):
 
         self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
 
+    # User CAN access 'recovery password' view if logged in
     def test_recovery_password_permission_success(self):
         self.user.password_recovery_token = uuid.uuid4()
         self.user.recovery_timestamp = timezone.now()
@@ -132,7 +141,8 @@ class UserPermissionTest(BaseTestCase):
         response = self.client.post(url, data, headers=self.custom_headers, format='json')
         self.assertEqual(response.status_code, status.HTTP_200_OK)
 
-    def test_reset_password_request_permission_success(self):
+    # User CAN NOT access 'recovery password' view if NOT logged in
+    def test_recovery_password_permission_fail(self):
         self.userPW.password_recovery_token = uuid.uuid4()
         self.userPW.recovery_timestamp = timezone.now()
         self.userPW.save()
