003a7cac2b723b0d7973246430c726ab4079bf07
cephe <cephei.1313@gmail.com>
2024-09-20 16:01:23 +0200

    Reorder urlpatterns in user/urls.py

--- user/urls.py
+++ user/urls.py
@@ -4,10 +4,10 @@ from user.views import UserConfirmationEmailView, UserRegisterView, UserRecoverP
     UserLogoutView, UserRecoverPasswordRequestView
 
 urlpatterns = [
-    path(r'email-verification/', UserConfirmationEmailView.as_view(), name='user-confirmation-email'),
-    path(r'email-register/', UserRegisterView.as_view(), name='user-register'),
+    path(r'login/', UserLoginView.as_view(), name='user-login'),
+    path(r'logout/', UserLogoutView.as_view(), name='user-logout'),
+    path(r'register/', UserRegisterView.as_view(), name='user-register'),
     path(r'recover-password/', UserRecoverPasswordView.as_view(), name='recover-password'),
     path(r'recover-password-request/', UserRecoverPasswordRequestView.as_view(), name='recover-password-request'),
-    path(r'login/', UserLoginView.as_view(), name='user-login'),
-    path(r'logout/', UserLogoutView.as_view(), name='user-logout')
+    path(r'email-verification/', UserConfirmationEmailView.as_view(), name='user-confirmation-email')
 ]
